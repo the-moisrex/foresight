07ee829c22fc5b23a97f7b697c26530e3874ad67
M. Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2025-07-25 11:11:37 -1000

    Capslock rocks

--- apps/pen2mice/pen2mice.cpp
+++ apps/pen2mice/pen2mice.cpp
@@ -8,30 +8,31 @@ import foresight.main.utils;
 
 int main(int const argc, char const* const* argv) {
     using namespace foresight;
+    using namespace std::chrono_literals;
     using std::filesystem::path;
     using std::views::drop;
     using std::views::transform;
 
     auto args = std::span{argv, argv + argc} | drop(1) | transform_to<std::string_view>();
 
-    static constexpr auto mid_left = op & pressed{BTN_MIDDLE} & pressed{BTN_LEFT};
     static constexpr auto main_pipeline =
       context
       | led_status
       | keys_status                    // Save key presses
-      | on(op & op_not{pressed(KEY_CAPSLOCK)} & led_off(LED_CAPSL),
+      | on(op | pressed(KEY_CAPSLOCK) | led_off(LED_CAPSL),
            context                     // Sub-context will be removed
              | abs2rel(true)           // Convert Pen events into Mouse events if any
              | ignore_abs              // Ignore absolute movements
              | ignore_big_jumps        // Ignore big mouse jumps
              | ignore_fast_left_clicks // Ignore fast left clicks
              | ignore_init_moves       // Fix pen small moves
-             | update_mod(keys_status))
-      | mice_quantifier                // Quantify the mouse movements
-      | swipe_detector                 // Detects swipes
+           // | update_mod(keys_status)
+           )
+      | mice_quantifier // Quantify the mouse movements
+      | swipe_detector  // Detects swipes
       | on(pressed(BTN_RIGHT), context | ignore_big_jumps(10) | ignore_start_moves) // fix right click jumps
       | on(op & pressed{BTN_MIDDLE} & triple_click, emit(press(KEY_LEFTMETA, KEY_TAB)))
-      | on(mid_left,
+      | on(op & pressed{BTN_MIDDLE} & pressed{BTN_LEFT},
            context
              | on(swipe_right, emit(press(KEY_LEFTCTRL, KEY_LEFTMETA, KEY_RIGHT)))
              | on(swipe_left, emit(press(KEY_LEFTCTRL, KEY_LEFTMETA, KEY_LEFT)))
@@ -39,7 +40,9 @@ int main(int const argc, char const* const* argv) {
              | on(swipe_down, emit(press(KEY_LEFTCTRL, KEY_LEFTMETA, KEY_DOWN)))
              | ignore_mouse_moves)
       | ignore_adjacent_syns
-      | add_scroll(op | pressed(BTN_MIDDLE) | pressed(KEY_CAPSLOCK), emit + up(BTN_MIDDLE));
+      | update_mod(keys_status)
+      | add_scroll(op | pressed(BTN_MIDDLE) | pressed(KEY_CAPSLOCK), emit + up(BTN_MIDDLE))
+      | on(longtime_released(pressed(KEY_CAPSLOCK), 500ms), emit + up(KEY_CAPSLOCK) + press(KEY_CAPSLOCK));
 
     if (args.size() > 0) {
         constinit static auto pipeline =
