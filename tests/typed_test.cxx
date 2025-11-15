f5ab9e64e644ca0dd974579b1e07b46e0abb6702
The Moisrex <12122474+the-moisrex@users.noreply.github.com>
2025-11-15 03:53:55 -1000

    Add multi-typed test; #98

--- tests/typed_test.cxx
+++ tests/typed_test.cxx
@@ -4,10 +4,12 @@
 
 import foresight.mods;
 
-bool happened = false;         // NOLINT
+int happened = 0;              // NOLINT
 
 TEST(SearchTest, Basic) {
     using namespace foresight; // NOLINT(*-build-using-namespace)
+
+    happened = 0;
 
     (context
      | emit_all({
@@ -33,7 +35,44 @@ TEST(SearchTest, Basic) {
     })
      | search_engine
      | on(typed("test"), [] {
-           happened = true;
+           happened = 1;
        }))();
-    EXPECT_TRUE(happened);
+    EXPECT_TRUE(happened == 1);
+}
+
+TEST(SearchTest, Multi) {
+    using namespace foresight; // NOLINT(*-build-using-namespace)
+    happened = 0;
+
+    (context
+     | emit_all({
+       {.type = EV_KEY,      .code = KEY_T, .value = 1},
+       {.type = EV_SYN, .code = SYN_REPORT, .value = 0},
+       {.type = EV_KEY,      .code = KEY_T, .value = 0},
+       {.type = EV_SYN, .code = SYN_REPORT, .value = 0},
+
+       {.type = EV_KEY,      .code = KEY_E, .value = 1},
+       {.type = EV_SYN, .code = SYN_REPORT, .value = 0},
+       {.type = EV_KEY,      .code = KEY_E, .value = 0},
+       {.type = EV_SYN, .code = SYN_REPORT, .value = 0},
+
+       {.type = EV_KEY,      .code = KEY_S, .value = 1},
+       {.type = EV_SYN, .code = SYN_REPORT, .value = 0},
+       {.type = EV_KEY,      .code = KEY_S, .value = 0},
+       {.type = EV_SYN, .code = SYN_REPORT, .value = 0},
+
+       {.type = EV_KEY,      .code = KEY_T, .value = 1},
+       {.type = EV_SYN, .code = SYN_REPORT, .value = 0},
+       {.type = EV_KEY,      .code = KEY_T, .value = 0},
+       {.type = EV_SYN, .code = SYN_REPORT, .value = 0},
+    })
+     | search_engine
+     | on(typed("test"),
+          [] {
+              ++happened;
+          })
+     | on(typed("es"), [] {
+           ++happened;
+       }))();
+    EXPECT_TRUE(happened == 2);
 }
